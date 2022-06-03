6081fdf6fd75176c7ca7b201185ec0ae52b92c81
openwrtdiy <57249132+openwrtdiy@users.noreply.github.com>
2022-06-03 19:13:09 +0800

    luci-app-pppoe-user: Update to 2022-06-03

--- luci-app-pppoe-user/luasrc/controller/pppoe-user.lua
+++ luci-app-pppoe-user/luasrc/controller/pppoe-user.lua
@@ -4,10 +4,10 @@ function index()
 	if not nixio.fs.access("/etc/config/pppoe-user") then
 		return
 	end
-	entry({"admin", "services", "pppoe-user"}, alias("admin", "services", "pppoe-user", "authlog"), _("PPPoE User Management"), 5)
-	entry({"admin", "services", "pppoe-user", "authlog"}, cbi("pppoe-user/authlog"), _("Auth Log"), 10).leaf = true
+	entry({"admin", "services", "pppoe-user"}, alias("admin", "services", "pppoe-user", "pppoelog"), _("PPPoE User Management"), 5)
+	entry({"admin", "services", "pppoe-user", "authlog"}, cbi("pppoe-user/pppoelog"), _("PPPoE Log"), 10).leaf = true
 	entry({"admin", "services", "pppoe-user", "user"}, cbi("pppoe-user/user"), _("User Manager"), 20).leaf = true
 	entry({"admin", "services", "pppoe-user", "speed"}, cbi("pppoe-user/speed"), _("Traffic Control"), 30).leaf = true
 	entry({"admin", "services", "pppoe-user", "online"}, cbi("pppoe-user/online"), _("Online User"), 40).leaf = true
-	entry({"admin", "services", "pppoe-user", "status"}, cbi("pppoe-user/status"), _("User Status"), 50).leaf = true
+	entry({"admin", "services", "pppoe-user", "status"}, cbi("pppoe-user/status"), _("User Status"), 50).leaf = false
 end
