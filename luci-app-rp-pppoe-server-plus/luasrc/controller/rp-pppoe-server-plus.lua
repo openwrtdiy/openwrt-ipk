304123b1c0e4612d7b7e6c29e4c56b93f54499ba
openwrtdiy <57249132+openwrtdiy@users.noreply.github.com>
2022-06-23 13:27:42 +0800

    Update rp-pppoe-server-plus.lua

--- luci-app-rp-pppoe-server-plus/luasrc/controller/rp-pppoe-server-plus.lua
+++ luci-app-rp-pppoe-server-plus/luasrc/controller/rp-pppoe-server-plus.lua
@@ -4,10 +4,10 @@ function index()
 	if not nixio.fs.access("/etc/config/pppoe-server") then
 		return
 	end
-	entry({"admin", "services", "rp-pppoe-server-plus"}, alias("admin", "services", "rp-pppoe-server-plus", "server"), _("PPPoE Server"), 60).dependent = true
-	entry({"admin", "services", "rp-pppoe-server-plus", "server"}, cbi("rp-pppoe-server-plus/server"), _("General Settings"),10).leaf = true
-	entry({"admin", "services", "rp-pppoe-server-plus", "pppd"}, cbi("rp-pppoe-server-plus/pppd"),_("Advanced Settings"),20).leaf = true
-	entry({"admin", "services", "rp-pppoe-server-plus", "firewall"}, cbi("rp-pppoe-server-plus/firewall"),_("Firewall Settings"),30).leaf = true
-	entry({"admin", "services", "rp-pppoe-server-plus", "log"}, form("rp-pppoe-server-plus/log"), _("Logging"), 40).leaf = true
-	entry({"admin", "services", "rp-pppoe-server-plus", "online"}, cbi("rp-pppoe-server-plus/online"),_("Online"),50).leaf = true
+	entry({"admin", "services", "rp-pppoe-server-plus"}, alias("admin", "services", "rp-pppoe-server-plus", "server"), _("PPPoE Server"), 99).dependent = true
+	entry({"admin", "services", "rp-pppoe-server-plus", "server"}, cbi("rp-pppoe-server-plus/server"), _("General Settings"),1).leaf = true
+	entry({"admin", "services", "rp-pppoe-server-plus", "pppd"}, cbi("rp-pppoe-server-plus/pppd"),_("Advanced Settings"),2).leaf = true
+	entry({"admin", "services", "rp-pppoe-server-plus", "firewall"}, cbi("rp-pppoe-server-plus/firewall"),_("Firewall Settings"),3).leaf = true
+	entry({"admin", "services", "rp-pppoe-server-plus", "log"}, form("rp-pppoe-server-plus/log"), _("Logging"), 4).leaf = true
+	entry({"admin", "services", "rp-pppoe-server-plus", "online"}, form("rp-pppoe-server-plus/online"),_("Online"),5).leaf = true
 end
