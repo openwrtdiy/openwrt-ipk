179429f4ba0b2b4eb09360bd87c3d515f6873c12
openwrtdiy <57249132+openwrtdiy@users.noreply.github.com>
2022-06-30 01:31:59 +0800

    Update rp-pppoe-client.lua

--- luci-app-rp-pppoe-client/luasrc/controller/rp-pppoe-client.lua
+++ luci-app-rp-pppoe-client/luasrc/controller/rp-pppoe-client.lua
@@ -6,9 +6,10 @@ function index()
 	end
 	entry({"admin", "status", "userstatus"}, alias("admin", "status", "userstatus", "onlineuser"), _("PPPoE User Status"), 999)
 	entry({"admin", "status", "userstatus", "onlineuser"}, form("rp-pppoe-client/onlineuser"), _("Online User"), 1).leaf = true
-	entry({"admin", "status", "userstatus", "downtimeuser"}, form("rp-pppoe-client/downtimeuser"), _("Downtime User"), 2).leaf = true
-	entry({"admin", "status", "userstatus", "onlinelog"}, form("rp-pppoe-client/onlinelog"), _("Online Log"), 3).leaf = true
-	entry({"admin", "status", "userstatus", "offlinelog"}, form("rp-pppoe-client/offlinelog"), _("Offline Log"), 4).leaf = true
+	entry({"admin", "status", "userstatus", "realtimetraffic"}, form("rp-pppoe-client/realtimetraffic"), _("Realtime Traffic"), 2).leaf = true
+	entry({"admin", "status", "userstatus", "downtimeuser"}, form("rp-pppoe-client/downtimeuser"), _("Downtime User"), 3).leaf = true
+	entry({"admin", "status", "userstatus", "onlinelog"}, form("rp-pppoe-client/onlinelog"), _("Online Log"), 4).leaf = true
+	entry({"admin", "status", "userstatus", "offlinelog"}, form("rp-pppoe-client/offlinelog"), _("Offline Log"), 5).leaf = true
 
 	entry({"admin", "services", "rp-pppoe-client"}, alias("admin", "services", "rp-pppoe-client", "user"), _("PPPoE User Management"), 99)
 	entry({"admin", "services", "rp-pppoe-client", "user"}, cbi("rp-pppoe-client/user"), _("User Manager")).leaf = true
