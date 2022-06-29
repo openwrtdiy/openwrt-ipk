45dc140eacd89892ef4c4b95b07c1bb8d7e4135e
openwrtdiy <57249132+openwrtdiy@users.noreply.github.com>
2022-06-30 00:34:11 +0800

    Update firewall.lua

--- luci-app-rp-pppoe-server-plus/luasrc/model/cbi/rp-pppoe-server-plus/firewall.lua
+++ luci-app-rp-pppoe-server-plus/luasrc/model/cbi/rp-pppoe-server-plus/firewall.lua
@@ -7,7 +7,6 @@ local m, s, o
 m = Map("pppoe-server")
 
 s = m:section(TypedSection, "firewall")
-s.addremove = false
 s.anonymous = true
 
 o = s:option(Flag, "isolation", translate("Disable Access Gateway"))
@@ -15,7 +14,14 @@ o.rmempty = false
 o.default = 1
 
 o = s:option(Value, "conntrackmax", translate("Maximum number of connections"))
-o.datatype = "port"
-o.default = 655350
+o.datatype = "range(16384,1048576)"
+o:value("16384", translate("16384 Memory 64MB"))
+o:value("32768", translate("32768 Memory 128MB"))
+o:value("65536", translate("65536 Memory 256MB"))
+o:value("131072", translate("131072 Memory 512MB"))
+o:value("262144", translate("262144 Memory 1GB"))
+o:value("524288", translate("524288 Memory 2GB"))
+o:value("1048576", translate("1048576 Memory 4GB"))
+o.default = 16384
 
 return m
