4a56b0b45eb5ea1f9d8fe94e59a02a118cb48032
openwrtdiy <57249132+openwrtdiy@users.noreply.github.com>
2022-07-10 19:29:14 +0800

    update

--- luci-app-rp-pppoe-client/luasrc/model/cbi/rp-pppoe-client/realtimetraffic.lua
+++ luci-app-rp-pppoe-client/luasrc/model/cbi/rp-pppoe-client/realtimetraffic.lua
@@ -10,10 +10,11 @@ local count = luci.sys.exec("grep -c enabled /etc/config/pppoe-client")
 t = f:section(Table, sessions, translate("Online User [ " .. count .. "]"))
 t:option(DummyValue, "username", translate("Username"))
 t:option(DummyValue, "macaddress", translate("MAC address"))
+t:option(DummyValue, "uptime", translate("Up Time"))
+t:option(DummyValue, "connect", translate("Connect"))
 t:option(DummyValue, "realtimeupload", translate("Realtime Upload"))
 t:option(DummyValue, "realtimedownload", translate("Realtime Download"))
 t:option(DummyValue, "totaluploads", translate("Total Uploads"))
 t:option(DummyValue, "totaldownloads", translate("Total Downloads"))
-t:option(DummyValue, "uptime", translate("Up Time"))
 
 return f
