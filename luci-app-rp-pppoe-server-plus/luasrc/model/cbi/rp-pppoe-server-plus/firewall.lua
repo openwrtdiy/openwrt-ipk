0df702cfb81553807f43ce52e41d6a60d3bc61ed
openwrtdiy <57249132+openwrtdiy@users.noreply.github.com>
2022-10-25 15:52:46 +0800

    Update firewall.lua

--- luci-app-rp-pppoe-server-plus/luasrc/model/cbi/rp-pppoe-server-plus/firewall.lua
+++ luci-app-rp-pppoe-server-plus/luasrc/model/cbi/rp-pppoe-server-plus/firewall.lua
@@ -15,15 +15,13 @@ o.default = 0
 
 o = s:option(Value, "conntrackmax", translate("Maximum number of connections"), translate("Adjust the maximum number of active connections"))
 o.datatype = "range(16384,4194304)"
-o:value("16384", translate("16384 Memory 64MB"))
-o:value("32768", translate("32768 Memory 128MB"))
-o:value("65536", translate("65536 Memory 256MB"))
-o:value("131072", translate("131072 Memory 512MB"))
-o:value("262144", translate("262144 Memory 1GB"))
-o:value("524288", translate("524288 Memory 2GB"))
-o:value("1048576", translate("1048576 Memory 4GB"))
-o:value("2097152", translate("2097152 Memory 8GB"))
-o:value("4194304", translate("4194304 Memory 16GB"))
+o:value("16384", translate("16384 Memory 1GB"))
+o:value("65536", translate("65536 Memory 4GB"))
+o:value("262144", translate("262144 Memory 8GB"))
+o:value("524288", translate("524288 Memory 16GB"))
+o:value("1048576", translate("1048576 Memory 32GB"))
+o:value("2097152", translate("2097152 Memory 64GB"))
+o:value("4194304", translate("4194304 Memory 128GB"))
 o.default = 16384
 
 return m
