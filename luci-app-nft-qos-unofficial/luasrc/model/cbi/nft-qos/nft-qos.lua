748409f74dfb88dd719caf2a4e888a58c07525b3
openwrtdiy <57249132+openwrtdiy@users.noreply.github.com>
2023-01-11 18:45:52 +0800

    Revert "Revert "luci-app-nft-qos-unofficial: Update to 2023-01-11""
    This reverts commit f47da72e8421c2ced484532fb97cc570ac50bd93.

--- luci-app-nft-qos-unofficial/luasrc/model/cbi/nft-qos/nft-qos.lua
+++ luci-app-nft-qos-unofficial/luasrc/model/cbi/nft-qos/nft-qos.lua
@@ -16,24 +16,18 @@ m = Map("nft-qos", translate("QoS over Nftables"))
 --
 -- Taboptions
 --
-s = m:section(TypedSection, "default", translate("NFT-QoS Settings"))
+s = m:section(TypedSection, "default", translate(""))
 s.addremove = false
 s.anonymous = true
 
-s:tab("limitopt", translate("Limit Rate Options"))
+s:tab("limitip", translate("IP Limit Rate Options"))
+s:tab("limitmac", translate("MAC Limit Rate Options"))
 
 --
 -- Enable IP address rate limiting
 --
-o = s:taboption("limitopt", Flag, "limit_ip_enable", translate("Limit Rate by IP Address"), translate("Enable Limit Rate Feature"))
+o = s:taboption("limitip", Flag, "limit_ip_enable", translate("Limit Rate by IP Address"), translate("Enable Limit Rate Feature"))
 o.default = limit_ip_enable or o.disabled
-o.rmempty = false
-
---
--- Enable Mac address rate limiting
---
-o = s:taboption("limitopt", Flag, "limit_mac_enable", translate("Limit Rate by Mac Address"), translate("Enable Limit Rate Feature"))
-o.default = limit_mac_enable or o.disabled
 o.rmempty = false
 
 --
@@ -47,23 +41,23 @@ if limit_ip_enable == "1" then
 	x.template = "cbi/tblsection"
 
 	o = x:option(Value, "hostname", translate("Hostname"))
+	o.placeholder = translate("Username")
 	o.datatype = "hostname"
 	o.default = ''
 
 	if has_ipv6 then
-		o = x:option(Value, "ipaddr", translate("IP Address (v4 / v6)"))
+		o = x:option(Value, "ipaddr", translate("IP Address"))
+		o.placeholder = translate("IP Address")
 	else
-		o = x:option(Value, "ipaddr", translate("IP Address (v4 Only)"))
+		o = x:option(Value, "ipaddr", translate("IP Address"))
 	end
 	o.datatype = "ipaddr"
 	if nixio.fs.access("/tmp/dhcp.leases") or nixio.fs.access("/var/dhcp6.leases") then
-		o.titleref = luci.dispatcher.build_url("admin", "status", "overview")
 	end
 
 	o = x:option(ListValue, "drate", translate("Download Rate"))
-	o.default = def_rate_dl or '128'
-	o.size = 4
-	o.datatype = "uinteger"
+	o:value("1100", "1 M")
+	o:value("5500", "5 M")
 	o:value("11000", "10 Mbps")
 	o:value("22000", "20 M")
 	o:value("33000", "30 M")
@@ -85,9 +79,8 @@ if limit_ip_enable == "1" then
 	o:value("1100000", "1000 Mbps")
 
 	o = x:option(ListValue, "urate", translate("Upload Rate"))
-	o.default = def_rate_ul or '128'
-	o.size = 4
-	o.datatype = "uinteger"
+	o:value("1100", "1 M")
+	o:value("5500", "5 M")
 	o:value("11000", "10 Mbps")
 	o:value("22000", "20 M")
 	o:value("33000", "30 M")
@@ -117,6 +110,13 @@ if limit_ip_enable == "1" then
 end
 
 --
+-- Enable Mac address rate limiting
+--
+o = s:taboption("limitmac", Flag, "limit_mac_enable", translate("Limit Rate by Mac Address"), translate("Enable Limit Rate Feature"))
+o.default = limit_mac_enable or o.disabled
+o.rmempty = false
+
+--
 -- Mac address speed limit
 --
 if limit_mac_enable == "1" then
@@ -127,17 +127,18 @@ if limit_mac_enable == "1" then
 	x.template = "cbi/tblsection"
 
 	o = x:option(Value, "hostname", translate("Hostname"))
+	o.placeholder = translate("Username")
 	o.datatype = "hostname"
 	o.default = ''
 
 	o = x:option(Value, "macaddr", translate("MAC Address"))
+	o.placeholder = translate("MAC Address")
 	o.rmempty = true
 	o.datatype = "macaddr"
 
 	o = x:option(ListValue, "drate", translate("Download Rate"))
-	o.default = def_rate_dl or '128'
-	o.size = 4
-	o.datatype = "uinteger"
+	o:value("1100", "1 M")
+	o:value("5500", "5 M")
 	o:value("11000", "10 Mbps")
 	o:value("22000", "20 M")
 	o:value("33000", "30 M")
@@ -157,17 +158,10 @@ if limit_mac_enable == "1" then
 	o:value("880000", "800 M")
 	o:value("990000", "900 M")
 	o:value("1100000", "1000 Mbps")
-
-	o = x:option(ListValue, "drunit", translate("Unit"))
-	o.default = def_unit or "kbytes"
-	o:value("bytes", "Bytes/s")
-	o:value("kbytes", "KBytes/s")
-	o:value("mbytes", "MBytes/s")
 
 	o = x:option(ListValue, "urate", translate("Upload Rate"))
-	o.default = def_rate_ul or '128'
-	o.size = 4
-	o.datatype = "uinteger"
+	o:value("1100", "1 M")
+	o:value("5500", "5 M")
 	o:value("11000", "10 Mbps")
 	o:value("22000", "20 M")
 	o:value("33000", "30 M")
@@ -188,7 +182,7 @@ if limit_mac_enable == "1" then
 	o:value("990000", "900 M")
 	o:value("1100000", "1000 Mbps")
 
-	o = x:option(ListValue, "urunit", translate("Unit"))
+	o = x:option(ListValue, "unit", translate("Unit"))
 	o.default = def_unit or "kbytes"
 	o:value("bytes", "Bytes/s")
 	o:value("kbytes", "KBytes/s")
