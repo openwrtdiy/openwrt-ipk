9bb1d124cc87f61092192350307ca7b5cfe7c325
openwrtdiy <57249132+openwrtdiy@users.noreply.github.com>
2023-01-30 22:07:15 +0800

    luci-app-pppoe-user: update to 2023-01-30

--- luci-app-pppoe-user/luasrc/controller/pppoe-user.lua
+++ luci-app-pppoe-user/luasrc/controller/pppoe-user.lua
@@ -10,6 +10,7 @@ function index()
 	entry({"admin", "status", "userstatus", "downtimeuser"}, form("pppoe-user/downtimeuser"), _("Downtime User"), 3).leaf = true
 	entry({"admin", "status", "userstatus", "onlinelog"}, form("pppoe-user/onlinelog"), _("Online Log"), 4).leaf = true
 	entry({"admin", "status", "userstatus", "offlinelog"}, form("pppoe-user/offlinelog"), _("Offline Log"), 5).leaf = true
+	entry({"admin", "status", "userstatus", "rate"}, template("nft-qos/rate"), _("Rate"), 6).leaf = true
 
 	entry({"admin", "services", "pppoe-user"}, alias("admin", "services", "pppoe-user", "user"), _("Broadband Account Management"), 99)
 	entry({"admin", "services", "pppoe-user", "user"}, cbi("pppoe-user/user"), _("User Manager")).leaf = true
