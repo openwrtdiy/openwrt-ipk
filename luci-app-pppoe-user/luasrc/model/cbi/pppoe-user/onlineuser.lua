8aa3390442de894044eae0ab09ec3c27ab11b5dc
openwrtdiy <57249132+openwrtdiy@users.noreply.github.com>
2023-02-10 21:01:40 +0800

    luci-app-pppoe-user: update to 2023-02-10

--- luci-app-pppoe-user/luasrc/model/cbi/pppoe-user/onlineuser.lua
+++ luci-app-pppoe-user/luasrc/model/cbi/pppoe-user/onlineuser.lua
@@ -32,6 +32,7 @@ t:option(DummyValue, "uptime", translate("Up Time"))
 kill = t:option(Button, "kill", translate("Forced Offline"))
 kill.inputstyle = "reset"
 function kill.write(t, s)
+    luci.util.execi("/usr/lib/sqm/run.sh stop $interface")
     luci.util.execi("rm -f " .. t.map:get(s, "session_file"))
     null, t.tag_error[t] = luci.sys.process.signal(t.map:get(s, "pid"), 9)
     luci.http.redirect(o.build_url("admin/status/userstatus/onlineuser"))
