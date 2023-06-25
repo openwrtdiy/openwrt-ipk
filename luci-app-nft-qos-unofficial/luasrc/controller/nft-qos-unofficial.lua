9d3b35c9dacb20580d0650407dbd938a71bc9dd8
openwrtdiy <57249132+openwrtdiy@users.noreply.github.com>
2023-06-25 18:44:35 +0800

    luci-app-nft-qos-unofficial: Update to 2023-06-25

--- luci-app-nft-qos-unofficial/luasrc/controller/nft-qos-unofficial.lua
+++ luci-app-nft-qos-unofficial/luasrc/controller/nft-qos-unofficial.lua
@@ -1,16 +1,16 @@
 -- Copyright 2018 Rosy Song <rosysong@rosinson.com>
 -- Licensed to the public under the Apache License 2.0.
 
-module("luci.controller.nft-qos", package.seeall)
+module("luci.controller.nft-qos-unofficial", package.seeall)
 
 function index()
-	if not nixio.fs.access("/etc/config/nft-qos") then
+	if not nixio.fs.access("/etc/config/nft-qos-unofficial") then
 		return
 	end
 
 	local e
 
-	e = entry({"admin", "status", "userstatus", "rate"}, template("nft-qos/rate"), _("Rate"), 9)
+	e = entry({"admin", "status", "userstatus", "rate"}, template("nft-qos-unofficial/rate"), _("Rate"), 9)
 	e.leaf = true
 	e.acl_depends = { "luci-app-nft-qos-unofficial" }
 
@@ -18,15 +18,15 @@ function index()
 	e.leaf = true
 	e.acl_depends = { "luci-app-nft-qos-unofficial" }
 
-	e = entry({"admin", "network", "nft-qos"}, cbi("nft-qos/nft-qos"), _("QoS over Nftables"), 100)
+	e = entry({"admin", "network", "nft-qos-unofficial"}, cbi("nft-qos-unofficial/nft-qos-unofficial"), _("QoS over Nftables"), 100)
 	e.leaf = true
 	e.acl_depends = { "luci-app-nft-qos-unofficial" }
 end
 
 function _action_rate(rv, n)
 	local c = nixio.fs.access("/proc/net/ipv6_route") and
-		io.popen("nft list chain inet nft-qos-monitor " .. n .. " 2>/dev/null") or
-		io.popen("nft list chain ip nft-qos-monitor " .. n .. " 2>/dev/null")
+		io.popen("nft list chain inet nft-qos-unofficial-monitor " .. n .. " 2>/dev/null") or
+		io.popen("nft list chain ip nft-qos-unofficial-monitor " .. n .. " 2>/dev/null")
 
 	if c then
 		for l in c:lines() do
@@ -38,7 +38,7 @@ function _action_rate(rv, n)
 				rv[#rv + 1] = {
 					rule = {
 						family = "inet",
-						table = "nft-qos-monitor",
+						table = "nft-qos-unofficial-monitor",
 						chain = n,
 						handle = 0,
 						expr = {
