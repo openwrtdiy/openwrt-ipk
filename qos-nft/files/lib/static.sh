83e91fa1031504d226d13b7cf240b589f069c3d6
openwrtdiy <57249132+openwrtdiy@users.noreply.github.com>
2024-10-10 14:43:07 +0800

    luci-app-qos-nft: Add IP speed limit switch

--- qos-nft/files/lib/static.sh
+++ qos-nft/files/lib/static.sh
@@ -8,41 +8,42 @@ qosdef_validate_static() {
 		'ip_type:maxlength(8)'
 }
 
-# append rule for static qos
-qosdef_append_rule_sta() { # <section> <operator> <default-unit> <default-rate>
-	local ipaddr unit rate burst connect
-	local operator=$2
+qosdef_append_rule_sta() {
+    local qos ipaddr rate unit burst connect
+    local operator=$2
 
-	config_get ipaddr $1 ipaddr
-	if [ "$operator" = "saddr" ]; then
-		config_get rate $1 urate $3
-		config_get unit $1 unit $4
-		config_get burst $1 burst $5
-		config_get connect $1 connect $6
-	else
-		config_get rate $1 drate $3
-		config_get unit $1 unit $4
-		config_get burst $1 burst $5
-	fi
+    config_get qos $1 qos
+    config_get ipaddr $1 ipaddr
+    if [ "$qos" != "1" ]; then
+        return
+    fi
 
-	[ -z "$ipaddr" ] && return
+    if [ "$operator" = "saddr" ]; then
+        config_get rate $1 urate $3
+        config_get unit $1 unit $4
+        config_get burst $1 burst $5
+        config_get connect $1 connect $6
+    else
+        config_get rate $1 drate $3
+        config_get unit $1 unit $4
+        config_get burst $1 burst $5
+    fi
 
-	# Convert units to KBytes (1 Mbps = 125 KBytes)
-	if [ "$unit" = "mbps" ]; then
-		rate=$((rate * 125))  # Convert to KBytes
-		unit="kbytes"
-	fi
-	
-	# If burst is empty and rate is greater than or equal to 10, set burst to rate / 10
-	if [ -z "$burst" ] && [ "$rate" -ge 10 ]; then
-		burst=$((rate / 10))
-	fi
+    [ -z "$ipaddr" ] && return
 
-	qosdef_append_rule_ip_limit $ipaddr $operator $unit $rate $burst $connect
+    if [ "$unit" = "mbps" ]; then
+        rate=$((rate * 125))
+        unit="kbytes"
+    fi
+    
+    if [ -z "$burst" ] && [ "$rate" -ge 10 ]; then
+        burst=$((rate / 10))
+    fi
+
+    qosdef_append_rule_ip_limit $ipaddr $operator $unit $rate $burst $connect
 }
 
-# append chain for static qos
-qosdef_append_chain_sta() { # <hook> <name> <section> <unit> <rate>
+qosdef_append_chain_sta() {
 	local hook=$1 name=$2
 	local config=$3 operator
 
@@ -62,7 +63,6 @@ qosdef_flush_static() {
 	qosdef_flush_table "$NFT_QOS_INET_FAMILY" qos-static
 }
 
-# static limit rate init
 qosdef_init_static() {
 	local hook_ul="prerouting" hook_dl="postrouting"
 
