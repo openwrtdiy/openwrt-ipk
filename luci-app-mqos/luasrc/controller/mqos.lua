ddf903c09e1ef47f44cd94cf7b242849c0e88ebf
openwrtdiy <57249132+openwrtdiy@users.noreply.github.com>
2024-10-20 11:06:02 +0800

    Remove some plugins

--- luci-app-mqos/luasrc/controller/mqos.lua
+++
@@ -1,9 +0,0 @@
-module("luci.controller.mqos", package.seeall)
-
-function index()
-	if not nixio.fs.access("/etc/config/mqos") then
-		return
-	end
-	
-	entry({"admin", "network", "mqos"}, cbi("mqos"), _("MQOS"), 10).dependent = true
-end
